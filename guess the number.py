ae8bc651ee30bd08940623fb8a4405bfc9e28093
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-11-21 14:07:23 -0700

    Create guess the number.py

---
+++ guess the number.py
@@ -0,0 +1,5 @@
+#This is a game where you must guess the secret random number!
+#Using input, ask the user to guess a number
+#A secret random number from 1 - 100 will be generated.
+#After each guess print either higher or lower
+#Using a loop, continue asking the user to guess until they get the correct answer
